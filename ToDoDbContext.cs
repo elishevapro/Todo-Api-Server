c9482e2dba8e5cb6cf5a52b799dd0bcf4ef73f53
elishevapro <elishevapro@gmail.com>
2024-03-06 22:09:45 +0200

    fix connection string again

--- ToDoDbContext.cs
+++ ToDoDbContext.cs
@@ -2,43 +2,24 @@
 using System.Collections.Generic;
 using Microsoft.EntityFrameworkCore;
 using Pomelo.EntityFrameworkCore.MySql.Scaffolding.Internal;
-using Microsoft.EntityFrameworkCore;
 
 namespace TodoApi;
 
 public partial class ToDoDbContext : DbContext
 {
-     protected readonly IConfiguration Configuration;
+    public ToDoDbContext()
+    {
+    }
 
-    public ToDoDbContext(IConfiguration configuration,DbContextOptions<ToDoDbContext> options)
+    public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
         : base(options)
     {
-        Configuration = configuration;
     }
-    // public ToDoDbContext(DbContextOptions<ToDoDbContext> options)
-    //     : base(options)
-    // {
-    // }
-    protected override void OnConfiguring(DbContextOptionsBuilder options)
-    {
-        // connect to mysql with connection string from app settings
-        var connectionString = Configuration.GetConnectionString("tododb");
-        options.UseMySql(connectionString, ServerVersion.AutoDetect(connectionString));
-    }
-        // protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-
-        // => optionsBuilder.UseMySql(Configuration.GetConnectionString("WebApiDatabase"), Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.36-mysql"));
-
-
-    // public ToDoDbContext()
-    // {
-    // }
-
-
 
     public virtual DbSet<Item> Items { get; set; }
-        // var connectionString = Configuration.GetConnectionString("WebApiDatabase");
 
+    protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        => optionsBuilder.UseMySql("server=localhost;user=root;password=ELI7eli7@;database=tododb", Microsoft.EntityFrameworkCore.ServerVersion.Parse("8.0.36-mysql"));
 
     protected override void OnModelCreating(ModelBuilder modelBuilder)
     {
